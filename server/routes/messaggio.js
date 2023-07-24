035e79f3394270a45bf0cde4a5b86992f8872ac3
Samuele Bouveret <samuele.bouveret@gmail.com>
2023-07-24 16:34:19 +0200

    Creazione APIs e testing per i messaggi

--- server/routes/messaggio.js
+++ server/routes/messaggio.js
@@ -1,27 +1,57 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const messaggioModel = require('../models/Messaggio');
+const utenteModel = require('../models/Utente');
+const annuncioModel = require('../models/Annuncio');
 
 const messaggioRouter = express.Router();
 
 messaggioRouter.get("/", async (req, res) => {
-    const _id = req.query._id;
-    try {
-        const utente = await utenteModel.findById(_id);
+    const mittente = req.query.mittente;
+    const destinatario = req.query.destinatario;
+    const annuncio = req.query.annuncio;
 
-        if (utente == null) {
-            res.status(404);
-            res.json({ message: "Messaggio non trovato" });
+    try {
+
+        const mittente_temp = await utenteModel.findById(mittente);
+
+        if (mittente_temp == null) {
+            res.status(400);
+            res.json({ message: "Mittente inesistente" });
             return
         }
 
-        res.json(utente);
+        const destinatario_temp = await utenteModel.findById(destinatario);
+
+        if (destinatario_temp == null) {
+            res.status(400);
+            res.json({ message: "Destinatario inesistente" });
+            return
+        }
+
+        if (mittente == destinatario) {
+            res.status(409);
+            res.json({ message: "Il mittente non può essere il destinatario" });
+            return
+        }
+
+        const annuncio_temp = await annuncioModel.findById(annuncio);
+
+        if (annuncio_temp == null) {
+            res.status(400);
+            res.json({ message: "Annuncio inesistente" });
+            return
+        }
+
+        const messaggio = await messaggioModel.find({ mittente: { $eq: mittente }, destinatario: { $eq: destinatario }, annuncio: { $eq: annuncio } }).exec();
+
+        res.status(200);
+        res.json(messaggio);
 
     } catch (err) {
         res.json(err);
     }
 });
-
 
 messaggioRouter.post("/", async (req, res) => {
     const messaggio = new messaggioModel(req.body);
@@ -32,5 +62,28 @@ messaggioRouter.post("/", async (req, res) => {
         res.json(err);
     }
 });
+
+messaggioRouter.delete("/", async (req, res) => {
+    const mittente = req.body.mittente;
+    const destinatario = req.body.destinatario;
+    const annuncio = req.body.annuncio;
+    const testo = req.body.testo;
+
+    try {
+
+        if (mittente == null || destinatario == null || annuncio == null || testo == null){
+            res.status(400);
+            res.json({ message: "Messaggio non eliminato per mancanza parametri" });
+            return
+        }
+
+            await messaggioModel.deleteOne({ mittente: { $eq: mittente }, destinatario: { $eq: destinatario }, annuncio: { $eq: annuncio }, testo: { $eq: testo } });
+
+        res.json({ message: "Messaggio eliminato con successo" });
+    } catch (error) {
+        res.json(error);
+    }
+});
+
 
 module.exports = messaggioRouter;
--- server/routes/messaggio.test.js
+++ server/routes/messaggio.test.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const MessaggioModel = require('../models/Messaggio');
 const app = require('../index.js');
 
-
 describe('TESTING /api/v1/messaggio', () => {
 
   let connection;
@@ -35,12 +34,97 @@ describe('TESTING /api/v1/messaggio', () => {
       .set('Accept', 'application/json')
       .send({
         mittente: "64bd332f17104915f0a72e7d",
-        destinatario: "64bd4e024f269412fdb45d94",
+        destinatario: "6481a687a378ef79e54920eb",
         annuncio: "6481a70ca378ef79e54920ef",
-        testo: "Ciao, vorrei comprare il tuo prodotto.",
+        testo: "Ciao, sarei interessato ad acquistare il tuo prodotto.",
       })
       .expect(200)
   });
 
+  test('GET /api/v1/messaggio should respond with a 400 error - idMittente inesistente', async () => {
+    return request(app)
+      .get('/api/v1/messaggio?mittente=000000000000000000000000')
+      .expect(400)
+  });
+
+  test('GET /api/v1/messaggio should respond with a 400 error - idDestinatario inesistente', async () => {
+    return request(app)
+      .get('/api/v1/messaggio?mittente=64bd332f17104915f0a72e7d&destinatario=000000000000000000000000')
+      .expect(400)
+  });
+
+  test('GET /api/v1/messaggio should respond with a 400 error - idAnnuncio inesistente', async () => {
+    return request(app)
+      .get('/api/v1/messaggio?mittente=64bd332f17104915f0a72e7d&destinatario=6481a687a378ef79e54920eb&annuncio=000000000000000000000000')
+      .expect(400)
+  });
+
+  test('GET /api/v1/messaggio should respond with a 400 error - nessun parametro fornito', async () => {
+    return request(app)
+      .get('/api/v1/messaggio/')
+      .expect(400)
+  });
+
+  test('GET /api/v1/messaggio should respond with a 409 error - mittente == destinatario', async () => {
+    return request(app)
+      .get('/api/v1/messaggio?mittente=6481a687a378ef79e54920eb&destinatario=6481a687a378ef79e54920eb')
+      .expect(409)
+  });
+
+  test('GET /api/v1/messaggio should receive all messages from mittente to destinatario on an annuncio', async () => {
+    return request(app)
+      .get('/api/v1/messaggio?mittente=64bd332f17104915f0a72e7d&destinatario=6481a687a378ef79e54920eb&annuncio=6481a70ca378ef79e54920ef')
+      .expect(200)
+  });
+
+  test('DELETE /api/v1/messaggio should delete a messaggio', async () => {
+    return request(app)
+      .get('/api/v1/messaggio?mittente=64bd332f17104915f0a72e7d&destinatario=6481a687a378ef79e54920eb&annuncio=6481a70ca378ef79e54920ef')
+      .set('Accept', 'application/json')
+      .then((res) => {
+        return request(app)
+          .delete('/api/v1/messaggio')
+          .set('Accept', 'application/json')
+          .send({
+            mittente: "64bd332f17104915f0a72e7d",
+            destinatario: "6481a687a378ef79e54920eb",
+            annuncio: "6481a70ca378ef79e54920ef",
+            testo: "Ciao, sarei interessato ad acquistare il tuo prodotto.",
+          })
+          .expect(200);
+      });
+
+  });
+
+  test('DELETE /api/v1/messaggio should not delete a messaggio for missing parameter', async () => {
+    return request(app)
+      .get('/api/v1/messaggio')
+      .set('Accept', 'application/json')
+      .then((res) => {
+        return request(app)
+          .delete('/api/v1/messaggio')
+          .set('Accept', 'application/json')
+          .send({
+            mittente: "64bd332f17104915f0a72e7d",
+            destinatario: "6481a687a378ef79e54920eb",
+            annuncio: "6481a70ca378ef79e54920ef",
+          })
+          .expect(400);
+      });
+
+  });
+
+  test('DELETE /api/v1/messaggio should not delete a messaggio for missing parameters', async () => {
+    return request(app)
+      .get('/api/v1/messaggio')
+      .set('Accept', 'application/json')
+      .then((res) => {
+        return request(app)
+          .delete('/api/v1/messaggio')
+          .set('Accept', 'application/json')
+          .expect(400);
+      });
+
+  });
 
 });
