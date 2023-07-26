2f3f05247be0dfcefcb4239c0ad184f259b34e67
elgiabe <gabriele.fasoli@studenti.unitn.it>
2023-07-27 00:31:55 +0200

    implementazione modifica utente

--- client/src/App.js
+++ client/src/App.js
@@ -8,21 +8,23 @@ import { Navbar } from './components/navbar';
 import { HomePage } from './pages/home-page';
 import { Modifica } from './pages/modifica-annuncio';
 import { ChatUtente } from './pages/chat-utente';
+import {ModificaProf} from './pages/mod-prof';
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router> 
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/mieiAnnunci" element={<MieiAnnunci />}></Route>
           <Route path="/vetrina" element={<Vetrina />}></Route>
           <Route path="/auth" element={<Auth />}></Route>
-          <Route path="/addAnnuncio" element={<AddAnnuncio />}>Annuncio</Route>
+          <Route path="/addAnnuncio" element={<AddAnnuncio />}></Route>
           <Route path="/chatUtente/:id" element={<ChatUtente />}></Route>
           <Route path="/modifica-annuncio" element={<Modifica />}></Route>
+          <Route path="/mod-prof" element={<ModificaProf />}></Route>
         </Routes>
       </Router>
 
